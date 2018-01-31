e921d83adbd3b85d2ad84885678a2010af3f8d6e
Mehmet <mehmetg@users.noreply.github.com>
2018-01-31 14:44:26 -0800

    Adds ttl related members/methods to the WriteRequest class. (#3)
    * Adds ttl related members/methods to the WriteRequest class.
    
    * Fixes typo in method name.
    
    * Bumps version for release
    
    * Adds ttl to Publish request.
    * Addresses type safety and error reporting issues from PR.

--- satori-rtm-sdk-core/src/main/java/com/satori/rtm/model/PublishRequest.java
+++ satori-rtm-sdk-core/src/main/java/com/satori/rtm/model/PublishRequest.java
@@ -1,5 +1,7 @@
 package com.satori.rtm.model;
 
+import com.google.common.base.Preconditions;
+
 /**
  * Represents the body of a Protocol Data Unit (<strong>PDU</strong>) for a publish request.
  * <p>
@@ -20,12 +22,24 @@ public class PublishRequest<T> {
 
   private T message;
 
-  public PublishRequest() {
-  }
+  //TODO: Use separate generic type for the TTL message, in case message types are different.
+  private T ttl_message;
+
+  private Long ttl;
+
+  public PublishRequest() { }
 
   public PublishRequest(String channel, T message) {
     this.channel = channel;
     this.message = message;
+  }
+
+  public PublishRequest(String channel, T message, final long ttl, final T ttl_message) {
+    Preconditions.checkArgument(ttl > 0, String.format("ttl must be non negative: %s", ttl));
+    this.channel = channel;
+    this.message = message;
+    this.ttl = ttl;
+    this.ttl_message = ttl_message;
   }
 
   public String getChannel() {
@@ -34,5 +48,27 @@ public class PublishRequest<T> {
 
   public T getMessage() {
     return message;
+  }
+
+  public T getTtlMessage() {
+    return ttl_message;
+  }
+
+  public Long getTtl() {
+    return ttl;
+  }
+
+  public PublishRequest<T> withTtl(final long newTtl, final T newTtlMessage) {
+    return new PublishRequest<T>(channel, message, newTtl, newTtlMessage);
+  }
+
+  @Override
+  public String toString() {
+    return "PublishRequest{" +
+        "channel='" + channel + '\'' +
+        ", message=" + message +
+        ", ttl=" + ttl +
+        ", ttlMessage=" + ttl_message +
+        '}';
   }
 }
--- satori-rtm-sdk-core/src/main/java/com/satori/rtm/model/WriteRequest.java
+++ satori-rtm-sdk-core/src/main/java/com/satori/rtm/model/WriteRequest.java
@@ -1,5 +1,7 @@
 package com.satori.rtm.model;
 
+import com.google.common.base.Preconditions;
+
 /**
  * Represents the body of a Protocol Data Unit (<strong>PDU</strong>) for a write request.
  * <p>
@@ -20,6 +22,9 @@ public class WriteRequest<T> {
   private String channel;
   private T message;
   private String position;
+  private Long ttl;
+  //TODO: Use separate generic type for the TTL message, in case message types are different.
+  private T ttl_message;
 
   public WriteRequest() { }
 
@@ -33,6 +38,17 @@ public class WriteRequest<T> {
     this.position = position;
   }
 
+
+
+  public WriteRequest(String channel, T message, String position, final long ttl, final T ttl_message) {
+    Preconditions.checkArgument(ttl > 0, String.format("ttl must be non negative: %s", ttl));
+    this.channel = channel;
+    this.message = message;
+    this.position = position;
+    this.ttl = ttl;
+    this.ttl_message = ttl_message;
+  }
+
   public String getChannel() {
     return channel;
   }
@@ -43,5 +59,28 @@ public class WriteRequest<T> {
 
   public String getPosition() {
     return position;
+  }
+
+  public Long getTtl() {
+    return ttl;
+  }
+
+  public T getTtlMessage() {
+    return ttl_message;
+  }
+
+  public WriteRequest<T> withTtl(final long newTtl, final T newTtlMessage) {
+    return new WriteRequest<T>(channel, message, position, newTtl, newTtlMessage);
+  }
+
+  @Override
+  public String toString() {
+    return "WriteRequest{" +
+        "channel='" + channel + '\'' +
+        ", message=" + message +
+        ", position='" + position + '\'' +
+        ", ttl=" + ttl +
+        ", ttlMessage=" + ttl_message +
+        '}';
   }
 }
